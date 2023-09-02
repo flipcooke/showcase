f7ca2763e7decc642aa15269500ffbc5495e1c55
flip <flipcooke@gmail.com>
2023-09-02 22:51:00 +0100

    create cols, link to image and description

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Phil Cooke")
+    content = """
+    Hello and welcome to my home page. I am a chartered accountant and a developer. I started my ICT career in 2023,
+    after having worked as an accountant for 16 years. With expertise in both industries enables me to provide 
+    ICT solutions to financial problems. 
+    """
+    st.info(content)
