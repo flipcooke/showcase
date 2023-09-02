a1f3e8c31961a9d4578954a873500e4da4783b43
flip <flipcooke@gmail.com>
2023-09-02 23:05:43 +0100

    add description wording under new columns

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-
 col1, col2 = st.columns(2)
 
 with col1:
@@ -12,7 +11,14 @@ with col2:
     st.title("Phil Cooke")
     content = """
     Hello and welcome to my home page. I am a chartered accountant and a developer. I started my ICT career in 2023,
-    after having worked as an accountant for 16 years. With expertise in both industries enables me to provide 
-    ICT solutions to financial problems. 
+after having worked as an accountant for 16 years. With expertise in both industries enables me to provide 
+ICT solutions to financial problems. 
     """
     st.info(content)
+
+introMsg = """
+Included below are a list of projects I have worked on in my own time for self development. 
+Please feel free to browse the below Python apps.
+"""
+st.write(introMsg)
+
